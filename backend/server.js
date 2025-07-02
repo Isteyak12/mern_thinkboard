d0df8b3d502add2e8a44671e94a7ea96c32299e7
Isteyak12 <isteyakislam12@gmail.com>
2025-07-01 23:09:38 -0400

    2nd manual commit

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,18 @@ import express from "express";
 // const express = require("express");
 const app = express();
 
+app.use(express.json());
+
+app.get("/api/notes", (req, res) => {
+  res.send("Hello from the server!");
+});
+
+app.post("/api/notes", (req, res) => {
+  res.send("Hello from the server!");
+});
+
 const PORT = 5001;
 
 app.listen(5001, () => {
-  console.log(`Server started on PORT ${PORT}`);
+  console.log(`Server started on PORT http://localhost:${PORT}`);
 });

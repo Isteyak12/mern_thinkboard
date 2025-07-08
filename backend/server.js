9131977c44560875ceabe08429de6293ec421121
Isteyak12 <isteyakislam12@gmail.com>
2025-07-08 01:53:56 -0400

    july8

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,11 @@ const app = express();
 app.use(express.json());
 
 app.get("/api/notes", (req, res) => {
-  res.send("Hello from the server!");
+  res.status(200).send("Hello from the server!");
 });
 
 app.post("/api/notes", (req, res) => {
-  res.send("Hello from the server!");
+  res.status(201).send("Hello from the server!");
 });
 
 const PORT = 5001;
